c75a2697d5d83b6e676f7c1292f9ea40c82a6c95
Louis Stiller <louisstiller@MacBook-Pro-von-Louis.local>
2026-08-03 17:54:22 +0200

    Modify hello GET endpoint to return JSON and accept name query parameter

--- src/main/java/de/louis/task_api/controller/HelloController.java
+++ src/main/java/de/louis/task_api/controller/HelloController.java
@@ -1,12 +1,14 @@
 package de.louis.task_api.controller;
 
+import de.louis.task_api.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("hello")
-    public String hello(){
-        return "Hallo aus meiner Task API";
+    public Greeting hello(@RequestParam(defaultValue = "Welt") String name){
+        return new Greeting("Hallo "+ name);
     }
 }
---
+++ src/main/java/de/louis/task_api/model/Greeting.java
@@ -0,0 +1,4 @@
+package de.louis.task_api.model;
+
+public record Greeting(String message) {
+}
