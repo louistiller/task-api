71279df35803d7d4b3a5ab0b152717fe87f13725
Louis Stiller <louisstiller@MBP-von-Louis.fritz.box>
2026-08-05 23:50:19 +0200

    Add OpenAPI documentation with Swagger UI

---
+++ src/main/java/de/louis/task_api/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package de.louis.task_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI taskApiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task API")
+                        .version("1.0")
+                        .description(
+                                "REST API for creating, reading, updating and deleting tasks"
+                        ));
+    }
+}
